f0b10a88d5b41ef7ab2a6e0388ffbe53cbfbf06b
Amie Wilt <amie.wilt@gmail.com>
2015-05-14 17:35:24 -0400

    renamed translator to server

--- server.rb
+++ server.rb
@@ -7,4 +7,14 @@ ROOT = File.dirname(__FILE__)
 server = WEBrick::HTTPServer.new(:Port => 8000, :DocumentRoot => "#{ROOT}/public")
 
 server.mount_proc '/content' do |req, res|
-  @page_title
+  @page_title = "Hack News Mock"
+  @hack = YAML.load_file("#{ROOT}/post.yml")
+  template = ERB.new(File.read("#{ROOT}/index.html.erb"))
+  res.body = template.result
+end
+
+trap 'INT' do
+  server.shutdown
+end
+
+server.start
